d5a8ac43e04b1f0268f614a93057949f06258eb6
JulianGonzalez <JulianAgustinGonzalez01@hotmail.com>
2022-01-18 23:25:06 -0300

    agrego ccs

--- src/components/_general/cartelera.js
+++ src/components/_general/cartelera.js
@@ -4,22 +4,22 @@ const Cartelera = () => {
     return (
         
         <div className='container-cards'>
-            <div>
+            <div className='box'>
                 <h3>Spiderman</h3>
                 <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcfwxobzyXFLnnPw9eiUMoq1K-ClEJNXRxkQ&usqp=CAU' className='img'></img>
                 <button>Ver</button>
             </div>
-            <div>
+            <div className='box'>
                 <h3>The Avengers</h3>
                 <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_awQEnvQFE7YRt4T8G7S89EEoAcknDoSIRQ&usqp=CAU' className='img'></img>
                 <button>Ver</button>
             </div>
-            <div>
+            <div className='box'>
                 <h3>The Incredibles</h3>
                 <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuYWo1SPp2b21bcCa71toFulaTjZdb0ZLb-Q&usqp=CAU' className='img'></img>
                 <button>Ver</button>
             </div>
-            <div>
+            <div className='box'>
                 <h3>Titans of the Pacifc</h3>
                 <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKjippZ9V7H2lve7HCNx_xHt8WfdJGWuyvVEIknCn_4s39i3kSeAxyaY9M0duo_h-0lmY&usqp=CAU' className='img'></img>
                 <button>Ver</button>
